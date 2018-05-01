5126100c844225409c82b4e210472e5a5f02dd90
Doug Hodson <doug@openeaagles.org>
2018-05-01 19:52:33 -0400

    updated examples

--- build/premake/examples.lua
+++ build/premake/examples.lua
@@ -1,6 +1,6 @@
 
 --------------------------------------------------------
--- Lua apps
+-- examples
 --------------------------------------------------------
 
 -- stock lua compiler
@@ -36,73 +36,53 @@ project "lua"
    if os.ishost("linux") then
       links { "dl", "readline" }
    else
-      defines { "_CONSOLE", }
+      defines { "_CONSOLE" }
+      links {"Ws2_32", "Winmm", "comctl32", "gdi32"}
+   end
+   links { "lua-5.3.4" }
+
+-- lua simple read-eval-print-loop
+project "repl-simple"
+   kind "ConsoleApp"
+   targetname "repl"
+   targetdir "../../examples/repl-simple"
+   debugdir "../../examples/repl-simple"
+   files {
+      "../../examples/repl-simple/**.c",
+      "../../examples/repl-simple/**.h"
+   }
+   includedirs { LuaIncPath }
+   libdirs     { LuaLibPath }
+   if os.ishost("linux") then
+      links { "dl", "readline" }
+   else
+      defines { "_CONSOLE" }
+      links {"Ws2_32", "Winmm", "comctl32", "gdi32"}
+   end
+   links { "lua-5.3.4" }
+
+-- lua stock read-eval-print-loop, extended with my own lib
+project "repl-extend"
+   kind "ConsoleApp"
+   targetname "repl"
+   targetdir "../../examples/repl-extend"
+   debugdir "../../examples/repl-extend"
+   files {
+      "../../examples/repl-extend/**.cpp",
+      "../../examples/repl-extend/**.c",
+      "../../examples/repl-extend/**.h"
+   }
+   includedirs { LuaIncPath }
+   libdirs     { LuaLibPath }
+   if os.ishost("linux") then
+      links { "dl", "readline" }
+   else
+      defines { "_CONSOLE" }
       links {"Ws2_32", "Winmm", "comctl32", "gdi32"}
    end
    links { "lua-5.3.4" }
 
 --[[
--- lua simple read-eval-print-loop
-project "lua-repl-simple"
-   kind "ConsoleApp"
-   targetname "repl"
-   targetdir "../../lua-repl-simple"
-   debugdir "../../lua-repl-simple"
-   files {
-      "../../lua-repl-simple/**.c",
-      "../../lua-repl-simple/**.h"
-   }
-   includedirs { Dep3RDPartyRoot .. "/src/lua-5.3.4" }
-   libdirs     { Dep3rdPartyLibPath }
-   defines { "_CONSOLE", }
-   filter "Release32"
-      links { "lua-5.3.4" }
-      links {"Ws2_32", "Winmm", "comctl32", "gdi32"}
-   filter "Debug32"
-      links { "lua-5.3.4_d" }
-      links {"Ws2_32", "Winmm", "comctl32", "gdi32"}
-
--- lua stock read-eval-print-loop, extended with my own lib
-project "lua-repl-extend"
-   kind "ConsoleApp"
-   targetname "repl"
-   targetdir "../../lua-repl-extend"
-   debugdir "../../lua-repl-extend"
-   files {
-      "../../lua-repl-extend/**.cpp",
-      "../../lua-repl-extend/**.c",
-      "../../lua-repl-extend/**.h"
-   }
-   includedirs { Dep3RDPartyRoot .. "/src/lua-5.3.4" }
-   libdirs     { Dep3rdPartyLibPath }
-   defines { "_CONSOLE", "LUA_COMPAT_MODULE" }
-   filter "Release32"
-      links { "lua-5.3.4" }
-      links {"Ws2_32", "Winmm", "comctl32", "gdi32"}
-   filter "Debug32"
-      links { "lua-5.3.4_d" }
-      links {"Ws2_32", "Winmm", "comctl32", "gdi32"}
-
--- lua mylib C++ lib extension as a DLL
-project "lua-mylib-dll"
-   kind "SharedLib"
-   targetname "mylib"
-   targetdir "../../lua-mylib-dll"
-   debugdir "../../lua-mylib-dll"
-   files {
-      "../../lua-mylib-dll/**.cpp",
-      "../../lua-mylib-dll/**.c",
-      "../../lua-mylib-dll/**.h"
-   }
-   targetsuffix ""
-   libdirs     { "../../lua-mylib-dll/lualib" }
-   defines { "_CONSOLE", "LUA_COMPAT_MODULE" }
-   filter "Release32"
-      links { "lua53" }
-      links {"Ws2_32", "Winmm", "comctl32", "gdi32"}
-   filter "Debug32"
-      links { "lua53" }
-      links {"Ws2_32", "Winmm", "comctl32", "gdi32"}
 
 -- lua test to interact with C++ code
 project "lua-test1"
--- examples/mylib-dll/README.md
+++
@@ -1,6 +0,0 @@
-
-Run the REPL application and execute the following code to exercise extension:
-
-    x = require "mylib"
-    x.str()
-    x.sin(1)
