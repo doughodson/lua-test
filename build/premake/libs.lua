f817af4b4aa2351157dccab1892824a3f689b5ff
Doug Hodson <doug@openeaagles.org>
2018-05-01 21:42:46 -0400

    updated example

--- build/premake/libs.lua
+++ build/premake/libs.lua
@@ -5,13 +5,13 @@ project "lua-5.3.4"
    -- destination directory for compiled binary target
    targetdir(LuaLibPath)
    files {
-      "../../lua-5.3.4/**.h",
-      "../../lua-5.3.4/**.hpp",
-      "../../lua-5.3.4/**.c"
+      "../../lua-5.3.4/src/**.h",
+      "../../lua-5.3.4/src/**.hpp",
+      "../../lua-5.3.4/src/**.c"
    }
    excludes {
-      "../../lua-5.3.4/lua.c",
-      "../../lua-5.3.4/luac.c"
+      "../../lua-5.3.4/src/lua.c",
+      "../../lua-5.3.4/src/luac.c"
    }
    if os.ishost("linux") then
       defines { "LUA_COMPAT_MODULE_5_2", "LUA_USE_LINUX" }
--- examples/repl-cpp/main.cpp
+++ examples/repl-cpp/main.cpp
@@ -8,30 +8,7 @@ extern "C" {
 #include "lualib.h"
 }
 
-
-//---------------------------------------------------------------------------------
-// Additions to stock REPL to extend
-//---------------------------------------------------------------------------------
-#include <lua.hpp>
-
-extern int luaopen_mylib(lua_State*);   // used to bind library with lua
-
-static const luaL_Reg mylibs[] = {
-  {"mylib", luaopen_mylib},
-  {NULL, NULL}
-};
-
-LUALIB_API void luaL_openmylibs (lua_State *L) {
-  const luaL_Reg *lib;
-  /* "require" functions from 'loadedlibs' and set results to global table */
-  for (lib = mylibs; lib->func; lib++) {
-    luaL_requiref(L, lib->name, lib->func, 1);
-    lua_pop(L, 1);  /* remove lib */
-  }
-}
-
-//---------------------------------------------------------------------------------
-//---------------------------------------------------------------------------------
+#include "mylib.hpp"
 
 int main()
 {
