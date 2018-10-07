6a91c6b83113e3949bad644baab6d63634a022da
Doug Hodson <doug@sidechannel.net>
2018-10-07 14:26:13 -0400

    updated premake for vs2017

--- build/premake/run-premake.cmd
+++ build/premake/run-premake.cmd
@@ -1,7 +1,6 @@
 rem
 rem delete old solution/workspace files
 rem
-rmdir /S /Q ..\vs2015
 rmdir /S /Q ..\vs2017
 rmdir /S /Q ..\obj
 rem
@@ -11,6 +10,5 @@ del ..\..\lib\*.lib
 rem
 rem create new solution/workspace files
 rem
-premake5-alpha12.exe --os=windows --file=premake5.lua vs2015
 premake5-alpha12.exe --os=windows --file=premake5.lua vs2017
 
