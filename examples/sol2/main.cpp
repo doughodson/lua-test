98099bb6a3979ae0bc7dd0852f857fa4e6dc92a0
Doug Hodson <doug@openeaagles.org>
2019-01-29 09:57:57 -0500

    adding more sol2 test examples

--- examples/sol2/main.cpp
+++ examples/sol2/main.cpp
@@ -1,8 +1,10 @@
 
 void test1();
 void test2();
+void test3();
+void test4();   // usertype
 
 int main()
 {
-   test2();
+   test4();
 }
---
+++ examples/sol2/test3/test3.cpp
@@ -0,0 +1,44 @@
+
+#include <sol.hpp>
+#include <iostream>
+
+#include "player.hpp"
+
+void test3 ()
+{
+   std::cout << "Lua integration test 3\n";
+   std::cout << "----------------------\n";
+
+   sol::state lua;
+
+        // note that you can set a
+        // userdata before you register a usertype,
+        // and it will still carry
+        // the right metatable if you register it later
+
+        // set a variable "p2" of type "player" with 0 ammo
+        lua["p2"] = player(0);
+
+        // make usertype metatable
+        lua.new_usertype<player>( "player",
+
+                // 3 constructors
+                sol::constructors<player(), player(int), player(int, int)>(),
+
+                // typical member function that returns a variable
+                "shoot", &player::shoot,
+                // typical member function
+                "boost", &player::boost,
+
+                // gets or set the value using member variable syntax
+                "hp", sol::property(&player::get_hp, &player::set_hp),
+
+                // read and write variable
+                "speed", &player::speed,
+                // can only read from, not write to
+                "bullets", sol::readonly( &player::bullets )
+        );
+
+        lua.script_file("test3.lua");
+}
+
