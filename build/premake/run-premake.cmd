dc293b99fcd8cfcb05cd011c2e6d52dc0678caca
Doug Hodson <doug@sidechannel.net>
2019-06-08 21:10:26 -0400

    updated premake

--- build/premake/run-premake.cmd
+++ build/premake/run-premake.cmd
@@ -2,6 +2,7 @@ rem
 rem delete old solution/workspace files
 rem
 rmdir /S /Q ..\vs2017
+rmdir /S /Q ..\vs2019
 rmdir /S /Q ..\obj
 rem
 rem delete old compiled libraries
@@ -10,5 +11,7 @@ del ..\..\lib\*.lib
 rem
 rem create new solution/workspace files
 rem
-premake5-alpha13.exe --os=windows --file=premake5.lua vs2017
+premake5-alpha14.exe --os=windows --file=premake5.lua vs2017
+premake5-alpha14.exe --os=windows --file=premake5.lua vs2019
+
 
